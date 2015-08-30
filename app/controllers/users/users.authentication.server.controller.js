585d7409883583b92b2f7ed229d08b996530c84a
walid <berrahal.walid07@gmail.com>
2015-08-30 04:49:47 +0200

    test2

--- app/controllers/users/users.authentication.server.controller.js
+++ app/controllers/users/users.authentication.server.controller.js
@@ -14,8 +14,7 @@ var _ = require('lodash'),
  */
 exports.signup = function(req, res) {
 	// For security measurement we remove the roles from the req.body object
-	//delete req.body.roles;
-	req.body.roles = ['user', 'admin'];
+	delete req.body.roles;
 
 	// Init Variables
 	var user = new User(req.body);
